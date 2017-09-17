be7273a7704830f3e9a8e8c4b78903af75020f15
deuscode <deuscode87@gmail.com>
2017-09-17 13:55:17 -0400

    add start timer function

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 // Declare global variables
 var mainTimer = document.getElementById('startTimer');
 var timerState = document.getElementById('displayTimer');
+var currentTimer = document.getElementById('displayTimer').innerHTML;
+console.log(currentTimer);
 
 // Event handler for user clicks on main timer
-mainTimer.addEventListener('click', startTimer);
+mainTimer.addEventListener('click', activateTimer);
 
-function startTimer() {
+function activateTimer() {
     if (timerState.style.zIndex == '-1') {
         timerState.style.zIndex = '1';
     } else {
         timerState.style.zIndex = '-1';
     }
 
+}
+
+function startTimer() {
+    if (timerState.style.zIndex == '-1') {
+
+    }
 }
