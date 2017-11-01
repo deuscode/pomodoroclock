14e6f076da2a14940fa9e182a562ed465dab262a
deuscode <deuscode87@gmail.com>
2017-10-31 22:58:33 -0400

    add logic for timer adjustments

--- js/script.js
+++ js/script.js
@@ -1,29 +1,55 @@
 // Declare global variables
 var mainTimer = document.getElementById('startTimer');
 var timerState = document.getElementById('displayTimer');
-var currentTimer = document.getElementById('displayTimer').innerHTML;
+var breakState = document.getElementById('displayBreak');
 var count = parseInt(document.getElementById('displayTimer').innerHTML);
-var breakCount
+var breakCount = parseInt(document.getElementById('displayBreak').innerHTML)
 var alertSound = document.getElementById('alertsound');
+var timerAdd = document.getElementById('timerAdd');
+var timerSubtract = document.getElementById('timerSubtract');
+var breakAdd = document.getElementById('breakAdd');
+var breakSubtract = document.getElementById('breakSubtract');
 
 // alertSound.play();
 
 // Event handler for user clicks on main timer
 mainTimer.addEventListener('click', activateTimer);
 
-function activateTimer() {
+function activateTimer(event) {
     if (timerState.style.zIndex == '-1') {
         timerState.style.zIndex = '1';
+        breakState.style.zIndex = '1';
     } else {
         timerState.style.zIndex = '-1';
+        breakState.style.zIndex = '-1';
     }
-
+    event.preventDefault();
 }
 
-var startTimer = setInterval(function() {
-    if (timerState.style.zIndex == '1' || count === 0) {
-        clearInterval(startTimer);
-    } else {
-        count--;
+timerAdd.addEventListener('click', function(event) {
+        count += 1;
+        timerState.innerHTML = count;
+    event.preventDefault();
+});
+
+timerSubtract.addEventListener('click', function(event) {
+    if(count > 0) {
+        count -= 1;
+        timerState.innerHTML = count;
     }
-}, 1000);
+    event.preventDefault();
+});
+
+breakAdd.addEventListener('click', function(event) {
+    breakCount += 1;
+    breakState.innerHTML = breakCount;
+event.preventDefault();
+});
+
+breakSubtract.addEventListener('click', function(event) {
+if(breakCount > 0) {
+    breakCount -= 1;
+    breakState.innerHTML = breakCount;
+}
+event.preventDefault();
+});
